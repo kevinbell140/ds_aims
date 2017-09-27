91b40b75b16b0ecb2fa1f41f548b2ac7f6844d22
Initech9 <you@example.com>
2017-09-27 02:16:27 -0400

    Updated the welcome screen, login, and made a POST for a blog /CRUD

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+			
         /*
          * Package Service Providers...
          */
@@ -176,7 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+		
+        /*
+         * HTML Service Provider...
+         */
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
